0fcf7747529b4ed797fb92c7c750d2171087908a
Abhyudoy Chaki <abhyudoychaki@gmail.com>
2025-08-28 12:42:04 +0530

    Update frontendURL to use environment variable instead of hardcoded value

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -12,5 +12,5 @@ export const dbUrl: string = assertEnv(process.env.MONGODB_URL, 'MONGODB_URL');
 export const JwtSecret: string = assertEnv(process.env.JWT_SECRET, 'JWT_SECRET');
 export const saltRounds: string = assertEnv(process.env.SALT_ROUNDS, 'SALT_ROUNDS');
 export const port: string = assertEnv(process.env.PORT, 'PORT');
-export const frontendURL: string = "http://localhost:5173";
+export const frontendURL: string = assertEnv(process.env.FRONTEND_URL, 'FRONTEND_URL');
 export const SAVE_TIMEOUT: number = 1000; // in ms
